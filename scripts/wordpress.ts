afa7966ccfef59acdff53397d2063ff6216ddf3b
Patryk Rzucidlo (PTKDev) <ptkdev@gmail.com>
2021-03-31 17:17:50 +0200

    [Release] v1.0.20210331

--- scripts/wordpress.ts
+++ scripts/wordpress.ts
@@ -25,9 +25,9 @@ fs.writeFileSync(`${__dirname}/../dist/readme.txt`, html, "utf8");
 html = nunjucks.render(`${__dirname}/../wordpress/svelte_webcomponent_boilerplate.php`, { package: pkg });
 fs.writeFileSync(`${__dirname}/../dist/svelte_webcomponent_boilerplate.php`, html, "utf8");
 
-shell.mkdir("-p", `${__dirname}/../build/${pkg.name.replace("@ptkdev/", "")}-wordpress/`);
-shell.cp("-R", `${__dirname}/../dist/*`, `${__dirname}/../build/${pkg.name.replace("@ptkdev/", "")}-wordpress/`);
+shell.mkdir("-p", `${__dirname}/../build/${pkg.name.replace("@ptkdev/", "")}-wordpress-plugin/`);
+shell.cp("-R", `${__dirname}/../dist/*`, `${__dirname}/../build/${pkg.name.replace("@ptkdev/", "")}-wordpress-plugin/`);
 
-shell.exec(`zip -r -j dist/${pkg.name.replace("@ptkdev/", "")}-wordpress.zip ${__dirname}/../build/${pkg.name.replace("@ptkdev/", "")}-wordpress/`);
+shell.exec(`zip -r -j dist/${pkg.name.replace("@ptkdev/", "")}-wordpress-plugin.zip ${__dirname}/../build/${pkg.name.replace("@ptkdev/", "")}-wordpress-plugin/`);
 
 shell.rm("-r", `${__dirname}/../build`);
